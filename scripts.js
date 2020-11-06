4b31f5011e43ca3b128f250c529dd283d8f614d0
kylebryant78 <33849084+kylebryant78@users.noreply.github.com>
2020-11-06 10:13:14 +0000

    added all files

---
+++ scripts.js
@@ -0,0 +1,22 @@
+let random_descriptions_array = ["A beam of crackling energy", 
+"Purple blasts", 
+"Rainbow energy spears",
+"Green lasers",
+"A neon green ray",
+"A black, smokey arrow",
+"Bright beams of hellfire",
+"A beam of pure energy, it almost hurts the eye's to look directly at it",
+"A mixture of blues, from dark to light, take on a wave like substance",
+"A handful of firey balls",
+"Several purple arrow tips",
+"A rigid beam of ice",
+"An invisible force rips through the air, the only reason you notice the spell hit is the hot air between the warlock and the now crumpled over enemy",
+"An arcane tentale strikes from above the warlock"];
+
+function show_quote () {
+    let random = Math.floor(Math.random() * random_descriptions_array.length);
+    let random_quote = (random, random_descriptions_array[random]);
+    console.log(random_quote);
+    document.getElementById("description").innerHTML = random_quote;
+}
+show_quote();
