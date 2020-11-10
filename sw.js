86458e3aac9d1b6aeb33f0f223692b32a80ede1b
kylebryant78 <33849084+kylebryant78@users.noreply.github.com>
2020-11-10 12:44:19 +0000

    logo and bg image
    moved logo and bg image to their own folder
    changed background from url to jp2 image for speed test

--- sw.js
+++ sw.js
@@ -4,6 +4,7 @@ const assets = [
     '/index.html',
     '/scripts.js',
     '/styles.css',
+    '/images/textured\ background.jp2'
     'https://fonts.googleapis.com/css2?family=Oswald&family=Raleway&display=swap',
 ];
 
